bf735cf56fc0132c268ab3bc1a06f2c10d51ca86
minseok <Kms60685@ggmail.com>
2025-04-13 21:55:54 +0900

    courses

--- backend/src/courses/courses.controller.ts
+++ backend/src/courses/courses.controller.ts
@@ -1,17 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { CoursesService } from './courses.service';
+import { CoursesService, CourseWithTime } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly svc: CoursesService) {}
-
-  // GET /courses?room=090425-0
+  
   @Get()
-  async findAll(
-    @Query('room') room?: string,
-    @Query('day') day?: string,
-  ) {
-    // 둘 중 하나만 쓰거나, 없으면 전체 리턴
-    return this.svc.findCourses({ room, day });
+  async byRoom(
+    @Query('room') room: string,
+  ): Promise<CourseWithTime[]> {
+    return this.svc.findCoursesByRoom(room);
   }
 }
