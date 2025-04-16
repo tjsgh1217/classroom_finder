23ca46d7186bc6ae972fb321cdc73d6e2404d4c0
minseok <Kms60685@ggmail.com>
2025-04-17 01:59:05 +0900

    4/17일 courses

--- backend/src/courses/courses.controller.ts
+++ backend/src/courses/courses.controller.ts
@@ -4,11 +4,11 @@ import { CoursesService, CourseWithTime } from './courses.service';
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly svc: CoursesService) {}
-  
+
   @Get()
-  async byRoom(
+  async getByRoom(
     @Query('room') room: string,
   ): Promise<CourseWithTime[]> {
-    return this.svc.findCoursesByRoom(room);
+    return this.svc.findBlockByRoom(room);
   }
 }
