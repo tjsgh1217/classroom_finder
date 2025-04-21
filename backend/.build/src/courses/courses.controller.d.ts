e5e94f57b1e224dbec3b63bb70bfda02ce26ee6b
tjsgh1217 <tjsgh1217@gmail.com>
2025-04-21 15:38:43 +0900

    chore: remove .build and .serverless from Git

--- backend/.build/src/courses/courses.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { CoursesService, CourseWithTime } from './courses.service';
-export declare class CoursesController {
-    private readonly svc;
-    constructor(svc: CoursesService);
-    getByRoom(room: string): Promise<CourseWithTime[]>;
-}
